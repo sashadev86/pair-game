c8ee7b3a39c7634c8d3c9f678cb19fec991742fb
Aliaksandr <djvalentinoff@gmail.com>
2022-07-04 14:13:03 +0300

    feat: add tooltip info

--- js/main.js
+++ js/main.js
@@ -184,4 +184,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
     location.reload();
   });
+
+  // tippy start
+
+  tippy('.tippy-tooltip', {
+    content(reference) {
+      const id = reference.getAttribute('data-template');
+      const template = document.getElementById(id);
+      return template.innerHTML;
+    },
+    allowHTML: true,
+    arrow: true,
+    delay: [100, 800],
+    interactive: true,
+    animation: 'rotate',
+    theme: 'violet',
+  });
+
+  // tippy finish
 });
